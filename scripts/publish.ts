4e2221ea93530f188f8c16f7ca7d73f203b1f1a3
groupguanfang <1203970284@qq.com>
2025-07-25 16:28:21 +0800

    chore: update scripts

---
+++ scripts/publish.ts
@@ -0,0 +1,60 @@
+import { execSync } from "node:child_process"
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { logger } from 'tsdown'
+
+const dirname = fileURLToPath(new URL('.', import.meta.url));
+
+function publishNpmPackages() {
+  try {
+    logger.info(`Publishing @arkts/* packages...`)
+    const command = `pnpm -F "@arkts/*" publish ${process.argv.slice(2).join(' ')}`
+    logger.info(`Executing command: ${command}`)
+    execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
+    return true
+  } catch (error) {
+    logger.error(`Failed to publish @arkts/* packages: ${error instanceof Error ? error.message : String(error)}`)
+    console.error(error)
+    return false
+  }
+}
+
+function publishToVsce() {
+  try {
+    logger.info(`Publishing to VSCE...`)
+    const command = `pnpm -F vscode-naily-ets vsce ${process.argv.slice(2).includes('--dry-run') ? 'package' : 'publish'} ${process.argv.slice(2).filter(v => v !== '--dry-run').join(' ')}`
+    logger.info(`Executing command: ${command}`)
+    execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
+    return true
+  } catch (error) {
+    logger.error(`Failed to publish to VSCE: ${error instanceof Error ? error.message : String(error)}`)
+    console.error(error)
+    return false
+  }
+}
+
+function publishToOvsce() {
+  try {
+    if (process.argv.includes('--dry-run')) {
+      logger.warn(`Skipping OVSCE package in dry-run mode.`)
+      return true
+    }
+    logger.info(`Publishing to OVSCE...`)
+    const command = `pnpm -F vscode-naily-ets ovsx publish ${process.argv.slice(2).filter(v => v !== '--dry-run').join(' ')}`
+    logger.info(`Executing command: ${command}`)
+    execSync(command, { cwd: path.resolve(dirname, '..'), stdio: 'inherit' })
+    return true
+  } catch (error) {
+    logger.error(`Failed to publish to OVSCE: ${error instanceof Error ? error.message : String(error)}`)
+    console.error(error)
+    return false
+  }
+}
+
+;(async () => {
+  const isPass = publishNpmPackages()
+  if (!isPass) return
+  const isVscePass = publishToVsce()
+  if (!isVscePass) return
+  publishToOvsce()
+})()
